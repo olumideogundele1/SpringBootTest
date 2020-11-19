652a5b1ad6df261a2f2b7efdccf0d382abc16f4f
olumide30 <ogundeleolumuyiwaolumide@gmail.com>
2020-11-19 08:13:56 +0100

    added services

--- src/main/java/com/olumide/swifttest/service/EmployeeService.java
+++ src/main/java/com/olumide/swifttest/service/EmployeeService.java
@@ -1,4 +1,16 @@
 package com.olumide.swifttest.service;
 
+import com.olumide.swifttest.Employee;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface EmployeeService {
+
+    Flux<Employee> getAllEmployees();
+
+    Mono<Employee> getEmployeeById(Long id);
+
+    Mono<Employee> createOrUpdateEmployee(Employee employee);
+
+    void deleteEmployee(Long id);
 }
--- src/main/java/com/olumide/swifttest/service/impl/EployeeServiceImpl.java
+++ src/main/java/com/olumide/swifttest/service/impl/EployeeServiceImpl.java
@@ -1,8 +1,44 @@
 package com.olumide.swifttest.service.impl;
 
+import com.olumide.swifttest.Employee;
+import com.olumide.swifttest.repository.EmployeeRepository;
 import com.olumide.swifttest.service.EmployeeService;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Service
 public class EployeeServiceImpl  implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Flux<Employee> getAllEmployees() {
+
+        List<Employee> list = employeeRepository.findAll();
+
+        return Flux.fromIterable(list);
+    }
+
+    @Override
+    public Mono<Employee> getEmployeeById(Long id) {
+        Employee employee = employeeRepository.getOne(id);
+        return Mono.justOrEmpty(employee);
+    }
+
+    @Override
+    public Mono<Employee> createOrUpdateEmployee(Employee employee) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+
+    }
 }
